fe5e79c669ae2adc1f24e86172f29c000fc832ef
TheTrabin <beyondtears@gmail.com>
2020-08-16 19:06:58 -0700

    Moving things around and building up routers, adjusting migrations, setting up roles (I know, unnecessary), then setting up seeds. Next after running seeds will be setting up test, and pushing migrations with env=testing migrate to test db as well as seeds to env=testing, then to set up the tests. Will also test using postman to make sure everything is running correctly and debug to make sure all setups are functioning properly.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,27 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+/**
+ * /*
+ *   complete the middleware code to check if the user is logged in
+ *   before granting access to the next middleware/route handler
+ *
+ * @format
+ */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+	if (req.sessiono && req.session.loggedIn) {
+		next();
+	} else {
+		res.status(401).json({ you: 'shall not pass!' });
+	}
 };
+
+function authenticateInTheMiddle(role) {
+	return function (req, res, next) {
+		if (req.decodedJwt.role && req.decodedJwt.role === role) {
+			next();
+		} else {
+			res.status(401).json({ you: 'shall not pass!' });
+		}
+	};
+}
+
+module.exports = authenticateInTheMiddle;
---
+++ auth/restricted-middleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
+
+module.exports = (req, res, next) => {
+  const [authType, token] = req.headers.authorization.split(' ');
+  console.log('token: ', token);
+
+  if (token) {
+jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+if (err) {
+  res.status(401).json({you: "can't touch this (invalid token)"})
+} else {
+  req.decodedJwt = decodedToken;
+  next();
+}
+})
+  } else {
+    res.status(401).json({you: "have no power (no token included"});
+  }
+};
